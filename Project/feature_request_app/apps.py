79d03dfecc0271685274d4320d0ec3936b6a44a3
priyankajain18 <priyankajain.madeeasy@gmail.com>
2018-02-27 01:18:47 +0530

    Initial SetUp

---
+++ Project/feature_request_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FeatureRequestAppConfig(AppConfig):
+    name = 'feature_request_app'
