d32ce4f119fc7538065d128fc2a6e1f3963a8769
priyankajain18 <priyankajain.madeeasy@gmail.com>
2018-03-04 23:10:03 +0530

    Add Feature

--- feature_request/models.py
+++ feature_request/models.py
@@ -3,4 +3,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=75)
+    is_enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    name = models.CharField(max_length=75)
+    priority = models.PositiveSmallIntegerField(default=1)
+
+    class Meta:
+        unique_together = (('name', 'priority'), )
+
+    def __str__(self):
+        return self.name
+
+class Feature(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    target_date = models.DateTimeField(auto_now_add=False, auto_now=False)
+    product = models.ForeignKey(Product)
+    client = models.ForeignKey(Client)
+
+    def __str__(self):
+        return self.title
